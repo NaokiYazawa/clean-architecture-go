7ad525dca686cd91b9f3a2cfeb16e3a2953df2e2
NaokiYazawa <yazawa.naoki.01@gmail.com>
2022-02-22 09:38:40 +0900

    changed architecture and set environment variable

--- src/domain/repository/user.go
+++ src/domain/repository/user.go
@@ -9,6 +9,7 @@ import (
 // Data Access Interface
 // UserRepository Interface of an user repository
 // UserRepository is interface for infrastructure
+// Clean Architecture の依存性の順番を守るために interface（抽象）のみを定義し、実際の実装は infra 層で行う。
 type UserRepository interface {
 	Create(user *model.User) (*model.User, error)
 	ReadByID(id int) (*model.User, error)
